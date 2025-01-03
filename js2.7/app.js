4a5a394a877511864c2aefa25eb7de67ae03dec8
EvaKn20 <evaknazeva0@gmail.com>
2025-01-03 19:30:11 +0300

    app.js

--- js2.7/app.js
+++ js2.7/app.js
@@ -1,7 +1,6 @@
 
 let num = prompt('Задать количество чисел, которые нужно вывести');
 function squareNum (min,max){
-    let square;
     for(let i = min; i < max; i++){
         console.log(i ** 2);
     }
